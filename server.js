6cd57d099a6f889c2d5aa258bbfbf53e50136adf
MathildeChan <mathilde.gore61@gmail.com>
2020-12-08 15:52:09 +0100

    Adding js and css routes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const { static } = require('express');
 let express = require('express');
 let app = express();
 let port = 3000;
 
 //Préparation du serveur
 app.use(express.static(__dirname + '/www'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.listen(port, () =>{
     console.log("Le serveur est en route.");
