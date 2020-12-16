62137f4a446bee8c4971400663e17eed82e178a2
MathildeChan <mathilde.gore61@gmail.com>
2020-12-16 11:04:03 +0100

    Using post to get name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { static } = require('express');
 let express = require('express');
+let bodyparser = require('body-parser');
 let app = express();
 let port = 3000;
 
@@ -11,6 +12,10 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+//BODY-PARSER
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
 
 app.listen(port, () =>{
     console.log("Le serveur est en route.");
@@ -34,8 +39,15 @@ app.get('/blog', (req, res, next) => {
     res.render('blog.ejs');
 })
 
+app.post('/blog', (req,res,next)=> {
+    console.log(req.body.name);
+    res.render('blog.ejs');
+})
+
 //      RECEPTION DES ERREURS (404)
 app.use((req, res, next) => {
     res.status(404).render('error.ejs');
 })
+
+
 
